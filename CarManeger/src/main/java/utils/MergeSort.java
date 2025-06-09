168f4611622524ce183d3b4b3b5dc7a67cd1b487
MzhkiD <caulk.simplex-0y@icloud.com>
2025-06-09 12:05:16 +0300

    Update MergeSort.java

--- CarManeger/src/main/java/utils/MergeSort.java
+++ CarManeger/src/main/java/utils/MergeSort.java
@@ -1,30 +1,31 @@
 package utils;
 
 import model.Car;
-import java.util.List;
+import model.CarList;
 
 public class MergeSort {
-    public static void sort(List<Car> list) {
-        if (list.size() <= 1) return;
+        public static void sort(CarList<Car> list) {
+            if (list.size() <= 1) return;
 
-        int mid = list.size() / 2;
-        List<Car> left = list.subList(0, mid);
-        List<Car> right = list.subList(mid, list.size());
+            int mid = list.size() / 2;
 
-        sort(left);
-        sort(right);
-        merge(list, left, right);
-    }
+            CarList<Car> left = list.subList(0, mid);
+            CarList<Car> right = list.subList(mid, list.size());
 
-    private static void merge(List<Car> result, List<Car> left, List<Car> right) {
-        int i = 0, j = 0, k = 0;
-        while (i < left.size() && j < right.size()) {
-            if (left.get(i).getPrice() <= right.get(j).getPrice())
-                result.set(k++, left.get(i++));
-            else
-                result.set(k++, right.get(j++));
+            sort(left);
+            sort(right);
+            merge(list, left, right);
         }
-        while (i < left.size()) result.set(k++, left.get(i++));
-        while (j < right.size()) result.set(k++, right.get(j++));
-    }
-}
+
+            private static void merge(CarList<Car> result, CarList<Car> left, CarList<Car> right) {
+                int i = 0, j = 0, k = 0;
+                while (i < left.size() && j < right.size()) {
+                    if (left.get(i).getPrice() <= right.get(j).getPrice())
+                        result.set(k++, left.get(i++));
+                    else
+                        result.set(k++, right.get(j++));
+                }
+                while (i < left.size()) result.set(k++, left.get(i++));
+                while (j < right.size()) result.set(k++, right.get(j++));
+            }
+        }
